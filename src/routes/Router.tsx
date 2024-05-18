5d5771003599dc2530844e713414c3a4c5f17c5b
João Lopes <joaovlopesmartins@gmail.com>
2024-05-17 21:25:02 -0300

    chore: create and config route system

---
+++ src/routes/Router.tsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from 'react-router-dom'
+import App from '../App'
+
+export function Router() {
+  return (
+    <Routes>
+        <Route path="/" element={<App />}></Route>
+    </Routes>
+  )
+}
