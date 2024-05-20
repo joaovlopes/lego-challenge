f9357dc3f6ea2a16f6399b2cdc5d3f410fb05125
João Lopes <joaovlopesmartins@gmail.com>
2024-05-20 07:27:54 -0300

    feat: footer component refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './components/home/home.tsx'
 import Header from './components/header/header.tsx'
+import Footer from './components/footer/footer.tsx'
 import './styles/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header />
     <Home />
-  </React.StrictMode>,
+    <Footer />
+  </React.StrictMode>
 )
